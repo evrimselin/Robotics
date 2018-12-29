40ec8848b7a066a6a89af5c89a537d7b442a3e51
Evrim Selin Altinkaynak <36560832+evrimselin@users.noreply.github.com>
2018-12-29 19:33:58 +0800

    Add files via upload

---
+++ RRR_LinkParameters.m
@@ -0,0 +1,17 @@
+%Link Parameter of RRR Planar Manipulator
+a1=500; %mm
+a2=260; %mm
+a3=50; %mm
+a1s=a1*0.001; %m
+a2s=a2*0.001; %m
+a3s=a3*0.001; %m
+L1=0.246193; %in (m)Link 1 center of mass
+m1=0.556965; %mass of Link 1
+I1=0.0123571; %moment inertia of Link 1 about its center of mass
+L2=0.626297-0.5; %in (m)Link 2 center of mass
+m2=0.297765;  %mass of Link 2
+I2=0.00189339; %moment inertia of Link 2 about its center of mass
+L3=0.780098-0.76; %in (m)Link 3 center of mass
+m3=0.0528412;  %mass of Link 3
+I3=0.0000137671; %(kgm^2) moment inertia of Link 3 about its center of mass
+g=9.81;
